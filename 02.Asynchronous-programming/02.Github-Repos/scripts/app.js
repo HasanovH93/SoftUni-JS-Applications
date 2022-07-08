dbfdc8a330606dc9d753b1516d8fd5ed04cb0923
Hasan Hasanov <hasanovh14@gmail.com>
2022-07-08 22:43:13 +0200

    AJAX

--- 02.Asynchronous-programming/02.Github-Repos/scripts/app.js
+++ 02.Asynchronous-programming/02.Github-Repos/scripts/app.js
@@ -1,31 +1,27 @@
-function loadRepos() {
-  const username = document.getElementById("username").value;
-  const list = document.getElementById("repos");
+ async function loadRepos() {
+	const username = document.getElementById("username").value;
+	const list = document.getElementById("repos");
+   try {
+	const response = await fetch(`https://api.github.com/users/${username}/repos`)
 
-  fetch(`https://api.github.com/users/${username}/repos`)
-    .then(handleResponse)
-    .then(displayReops)
-    .catch(handleError);
-
-	function handleResponse(response){
-		if(response.ok == false){
-			throw new Error (`${response.status} ${response.statusText}`)
-		}
-     return response.json()
+	if (response.ok == false) {
+		throw new Error(`${response.status} ${response.statusText}`);
 	}
+	const data = await response.json();
 
-  function displayReops(data) {
-    list.innerHTML = "";
+	list.innerHTML = "";
 
-    for (let repo of data) {
-      list.innerHTML += `<li>
+	for (let repo of data) {
+		list.innerHTML += `<li>
             <a href="${repo.html_url}">
                 ${repo.full_name}
             </a>
         </li>`;
-    }
-  }
-  function handleError(error) {
-    list.innerHTML = `${error.massage}`;
-  }
+	}
+   }catch (error){
+	list.innerHTML = `${error.message}`;
+   }
+	
+
+
 }
