2b9c2cc9bd408757404c874e5e5b7f9d45e42e17
Hasan Hasanov <hasanovh14@gmail.com>
2022-07-08 22:35:34 +0200

    AJAX

--- 02.Asynchronous-programming/02.Github-Repos/scripts/app.js
+++ 02.Asynchronous-programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,31 @@
 function loadRepos() {
-	console.log("TODO...");
+  const username = document.getElementById("username").value;
+  const list = document.getElementById("repos");
+
+  fetch(`https://api.github.com/users/${username}/repos`)
+    .then(handleResponse)
+    .then(displayReops)
+    .catch(handleError);
+
+	function handleResponse(response){
+		if(response.ok == false){
+			throw new Error (`${response.status} ${response.statusText}`)
+		}
+     return response.json()
+	}
+
+  function displayReops(data) {
+    list.innerHTML = "";
+
+    for (let repo of data) {
+      list.innerHTML += `<li>
+            <a href="${repo.html_url}">
+                ${repo.full_name}
+            </a>
+        </li>`;
+    }
+  }
+  function handleError(error) {
+    list.innerHTML = `${error.massage}`;
+  }
 }
