119ed135426bfefde0c441343fb3179b7e7f9d44
Hasan Hasanov <hasanovh14@gmail.com>
2022-07-09 14:55:48 +0200

    AJAX - Exercise

--- 03.Asynchonous-programming - Exercise/02.Bus-Schedule/app.js
+++ 03.Asynchonous-programming - Exercise/02.Bus-Schedule/app.js
@@ -1,17 +1,33 @@
 function solve() {
+  const info = document.querySelector("#info span");
+  const departBtn = document.getElementById("depart");
+  const arriveBtn = document.getElementById("arrive");
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+  let stop = {
+    next: "depot",
+  };
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+  async function depart() {
+   departBtn.disabled = true;
+   const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+   const response = await fetch(url)
+   const data = await response.json()
+   stop = data
+   info.textContent = `Next stop ${stop.name}`
+   arriveBtn.disabled = false
+  }
 
-    return {
-        depart,
-        arrive
-    };
+  function arrive() {
+    info.textContent = `Arriving at ${stop.name}`
+    departBtn.disabled = false;
+    arriveBtn.disabled = true;
+  
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
