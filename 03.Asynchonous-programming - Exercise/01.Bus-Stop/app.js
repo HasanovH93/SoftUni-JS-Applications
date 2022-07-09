0d597e8f07ec98a2b36bd13732b37f3d4292d335
Hasan Hasanov <hasanovh14@gmail.com>
2022-07-09 13:56:25 +0200

    AJAX - Exercise

--- 03.Asynchonous-programming - Exercise/01.Bus-Stop/app.js
+++ 03.Asynchonous-programming - Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,31 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+  const stopId = document.getElementById("stopId").value;
+  const stopName = document.getElementById("stopName");
+  const list = document.getElementById("buses");
+
+  const url = `http://localhost:3030/jsonstore/bus/businfos/${stopId}`;
+
+  try {
+    list.textContent = ''
+    stopName.textContent = 'Loading....'
+    const response = await fetch(url);
+   
+    if (response.ok == false) {
+		throw new Error(`${response.status} ${response.statusText}`);
+	}
+    const data = await response.json();
+    stopName.textContent = data.name;
+
+    Object.entries(data.buses).forEach(bus => {
+        const liElement = document.createElement('li');
+        liElement.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`
+        list.appendChild(liElement)
+    })
+
+
+   
+
+  } catch (error) {
+    stopName.textContent = `Error`
+  }
 }
