7351f638b1fd4e300843048fc25e6ca53c9ff33d
Hasan Hasanov <hasanovh14@gmail.com>
2022-07-10 12:25:59 +0200

    Remote Data and Authentication

--- 04.Remote Data and Authentication/demo.js
+++ 04.Remote Data and Authentication/demo.js
@@ -1,20 +1,10 @@
-const data = {
-    'body': 'has hasicha has hasicha',
-    'title': 'Hasanov Hasan',
+document.getElementById('refreshBtn').addEventListener('click',getData)
+document.getElementById('createBtn').addEventListener('click',postData)
+
+
+function getData(){
 
 }
-async function start(){
-    const reposne = await fetch('http://localhost:3030/jsonstore/blog/posts',{
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    
-    });
-    const responseData = await reposne.json()
-    console.log(responseData)
+function postData(){
+
 }
-
-start()
-
