11259c77ff75e09661b0ae7746d99d58b6d2ea5e
Hasan Hasanov <hasanovh14@gmail.com>
2022-07-10 21:24:26 +0200

    Remote Data and Authentication

--- 04.Remote Data and Authentication/cookBook/src/login.js
+++ 04.Remote Data and Authentication/cookBook/src/login.js
@@ -1,23 +1,43 @@
-document.querySelector('form').addEventListener('submit',onSubmit)
+document.querySelector("form").addEventListener("submit", onSubmit);
 
+async function onSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
 
-async function onSubmit(event){
-    event.preventDefault();
-    const formData =  new FormData(event.target)
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-    const email = formData.get('email')
-    const password = formData.get('password')
-
-    try {
-
-        if(email == "" ) {
-            throw new Error('Email is required')
-        }
-        if(password == ""){
-            throw new Error('Password is required')
-        }
-        
-    } catch (error) {
-        alert(error.message)
+  try {
+    if (email == "") {
+      document.querySelector('[name="email"]').style.border = "1px solid red";
+      throw new Error("Email is required");
+    } 
+    if (password == "") {
+      document.querySelector('[name="password"]').style.border =
+        "1px solid red";
+      throw new Error("Password is required");
     }
+
+    const response = await fetch('http://localhost:3030/users/login',{
+        method: "POST",
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    })
+
+    if(response.ok == false){
+        const error = await response.json();
+        throw new Error(error.message)
+    }
+
+    const data = await response.json();
+    sessionStorage.setItem('accessToken',data.accessToken)
+    window.location = '/04.Remote%20Data%20and%20Authentication/cookbook/';
+  } catch (error) {
+    alert(error.message);
+  }
 }
