7826fef46227f44faf6ad14d8de806db890b734b
Hasan Hasanov <hasanovh14@gmail.com>
2022-07-11 20:12:09 +0200

    Remote Data And Authentication

--- 05.Remote Data and Authentication - Exercise/02.Phonebook/app.js
+++ 05.Remote Data and Authentication - Exercise/02.Phonebook/app.js
@@ -1,5 +1,31 @@
+const person = document.getElementById("person");
+const phone = document.getElementById("phone");
+const phoneBook = document.getElementById("phonebook");
+const url = "http://localhost:3030/jsonstore/phonebook";
+
 function attachEvents() {
-    console.log('TODO...');
+  document.getElementById("btnLoad").addEventListener("click", onLoad);
+  document.getElementById("btnCreate").addEventListener("click", onCreate);
+}
+async function onLoad() {
+  const response = await fetch(url);
+
+  const data = await response.json();
+
+  Object.values(data).forEach((x) => {
+    const { person, phone, _id } = x;
+    const li = createElement("li", `${person}: ${phone}`, phoneBook);
+    li.setAttribute("id", _id);
+  });
+}
+
+async function onCreate() {}
+
+function createElement(type, content, appender) {
+  const resultElement = document.createElement(type);
+  resultElement.textContent = content;
+  appender.appendChild(resultElement);
+  return resultElement;
 }
 
 attachEvents();
