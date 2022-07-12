5e1ad29cf826a7481ad591074f6309e83613f6f5
Hasan Hasanov <hasanovh14@gmail.com>
2022-07-12 15:31:53 +0200

    Remote Data And Authentication

--- 05.Remote Data and Authentication - Exercise/02.Phonebook/app.js
+++ 05.Remote Data and Authentication - Exercise/02.Phonebook/app.js
@@ -2,11 +2,11 @@ const person = document.getElementById("person");
 const phone = document.getElementById("phone");
 const phoneBook = document.getElementById("phonebook");
 const url = "http://localhost:3030/jsonstore/phonebook";
+const loadBtn = document.getElementById("btnLoad");
+loadBtn.addEventListener("click", onLoad);
+document.getElementById("btnCreate").addEventListener("click", onCreate);
 
-function attachEvents() {
-  document.getElementById("btnLoad").addEventListener("click", onLoad);
-  document.getElementById("btnCreate").addEventListener("click", onCreate);
-}
+function attachEvents() {}
 async function onLoad() {
   const response = await fetch(url);
 
@@ -17,12 +17,29 @@ async function onLoad() {
     const li = createElement("li", `${person}: ${phone}`, phoneBook);
     li.setAttribute("id", _id);
     const dltBtn = createElement("button", "Delete", li);
-    dltBtn.setAttribute('id','btnDelete')
-    dltBtn.addEventListener('click',onDelete)
+    dltBtn.setAttribute("id", "btnDelete");
+    dltBtn.addEventListener("click", onDelete);
   });
 }
 
-async function onCreate() {}
+async function onCreate() {
+  if (person.value !== "" || phone.value !== "") {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        person: person.value,
+        phone: phone.value,
+      }),
+    });
+    ul.innerHTML = ''
+    loadBtn.click();
+    person.value = "";
+    phone.value = "";
+  }
+}
 
 function createElement(type, content, appender) {
   const resultElement = document.createElement(type);
@@ -30,5 +47,7 @@ function createElement(type, content, appender) {
   appender.appendChild(resultElement);
   return resultElement;
 }
+
+async function onDelete() {}
 
 attachEvents();
