d004b1621d1b52f7384267564a28c551b0c05ae5
HasanovH93 <hasanovh14@gmail.com>
2022-10-02 12:50:27 +0200

    SPA Demo

--- 06.Single Page Aplications/demo/src/app.js
+++ 06.Single Page Aplications/demo/src/app.js
@@ -1,1 +1,28 @@
-console.log('its working')
+document.querySelectorAll("section").forEach((s) => (s.style.display = "none"));
+document.getElementById("homeView").style.display = "";
+
+document.querySelector("nav").addEventListener("click", onNavigate);
+
+const sections = {
+    
+}
+
+function onNavigate(event) {
+  if (event.target.tagName == "BUTTON") {
+    document
+      .querySelectorAll("section")
+      .forEach((s) => (s.style.display = "none"));
+
+    switch (event.target.id) {
+      case "homeBtn":
+        document.getElementById("homeView").style.display = "";
+        break;
+      case "catalogBtn":
+        document.getElementById("catalogView").style.display = "";
+        break;
+      case "aboutBtn":
+        document.getElementById("aboutView").style.display = "";
+        break;
+    }
+  }
+}
