a0696bd91ab72c89f1eda7a9caa89fd38e6bfc70
HasanovH93 <hasanovh14@gmail.com>
2022-10-08 14:48:11 +0200

    add create movie

--- 06.Single Page Aplications/Movies/client/src/create.js
+++ 06.Single Page Aplications/Movies/client/src/create.js
@@ -1,7 +1,35 @@
+import { homePage } from "./home.js";
 import { showView } from "./util.js";
 
 const section = document.querySelector("#add-movie");
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit)
 
 export function createPage(){
   showView(section)
+ }
+
+ async function onSubmit(event){
+  event.preventDefault()
+   const formData = new FormData(form);
+
+   const title = formData.get('title');
+   const description = formData.get('description');
+   const imgUrl = formData.get('imageUrl');
+  
+   await createMovie(title,description,imgUrl);
+   form.reset();
+   homePage();
+ }
+
+ async function createMovie(title,descritpion, img){
+  const user = JSON.parse(localStorage.getItem('user'));
+  const res = await fetch('http://localhost:3030/data/movies', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Authorization': user.accessToken
+    },
+    body: JSON.stringify({title,descritpion,img})
+  })
  }
