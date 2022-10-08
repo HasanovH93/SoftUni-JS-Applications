f68aeacb56f5f62d5077573f283b5237187c5f01
HasanovH93 <hasanovh14@gmail.com>
2022-10-08 22:40:09 +0200

    tests update

--- 07.Architecture and Testing/demo/test.js
+++ 07.Architecture and Testing/demo/test.js
@@ -1,5 +1,5 @@
-const { firefox } = require('playwright-firefox');
-const { expect } = require('chai')
+const { firefox } = require("playwright-firefox");
+const { expect } = require("chai");
 // (async () => {
 //     const browser = await firefox.launch();
 //     const page = await browser.newPage();
@@ -8,18 +8,57 @@ const { expect } = require('chai')
 //     await browser.close()
 // })();
 
-
 let browser, page; // Declare reusable variables
-describe('E2E tests', async function(done) {
-    this.timeout(5000);
+describe("E2E tests", async function (done) {
+  this.timeout(5000);
 
-  before(async () => { browser = await firefox.launch(); });
-  after(async () => { await browser.close(); });
-  beforeEach(async () => { page = await browser.newPage(); });
-  afterEach(async () => { await page.close(); }); 
+  before(async () => {
+    browser = await firefox.launch({ headless: false, slowMo: 500 });
+  });
+  after(async () => {
+    await browser.close();
+  });
+  beforeEach(async () => {
+    page = await browser.newPage();
+  });
+  afterEach(async () => {
+    await page.close();
+  });
 
-  it('works', async () => {
-   await page.goto('http://localhost:5500/Accordion');
-   await page.screenshot({ path: 'site.png' })
-  })
+  it("loads article titles", async () => {
+    await page.goto("http://localhost:5500/Accordion");
+    //    await page.click('text=More');
+    //    await page.screenshot({ path: 'site.png' })
+
+    await page.waitForSelector(".accordion");
+
+    const content = await page.textContent("#main");
+    expect(content).to.contain("Scalable Vector Graphics");
+    expect(content).to.contain("Open standard");
+    expect(content).to.contain("Unix");
+    expect(content).to.contain("ALGOL");
+  });
+
+  it("loads article titles", async () => {
+    await page.goto("http://localhost:5500/Accordion");
+
+    await page.click("text=More");
+    // await page.waitForSelector('.accordion')  
+    const visible = await page.isVisible(".accordion p");
+    expect(visible).to.be.true;
+  });
+
+  it("loads article titles", async () => {
+    await page.goto("http://localhost:5500/Accordion");
+
+    await page.click("text=More");
+    // await page.waitForSelector('.accordion')  
+    let visible = await page.isVisible(".accordion p");
+    expect(visible).to.be.true;
+
+    await page.click("text=Less");
+    visible = await page.isVisible(".accordion p");
+    expect(visible).to.be.false;
+
+  });
 });
