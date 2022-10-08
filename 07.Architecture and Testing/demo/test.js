ffcf088884bcc22c12cedf3ffa538fbc5ee315b9
HasanovH93 <hasanovh14@gmail.com>
2022-10-08 22:03:58 +0200

    install mocha and chai , first tests

--- 07.Architecture and Testing/demo/test.js
+++ 07.Architecture and Testing/demo/test.js
@@ -1,8 +1,25 @@
 const { firefox } = require('playwright-firefox');
-(async () => {
-    const browser = await firefox.launch();
-    const page = await browser.newPage();
-    await page.goto('https://melindabeauty.de/')
-    await page.screenshot({path: `example.png`});
-    await browser.close()
-})();
+const { expect } = require('chai')
+// (async () => {
+//     const browser = await firefox.launch();
+//     const page = await browser.newPage();
+//     await page.goto('https://melindabeauty.de/')
+//     await page.screenshot({path: `example.png`});
+//     await browser.close()
+// })();
+
+
+let browser, page; // Declare reusable variables
+describe('E2E tests', async function(done) {
+    this.timeout(5000);
+
+  before(async () => { browser = await firefox.launch(); });
+  after(async () => { await browser.close(); });
+  beforeEach(async () => { page = await browser.newPage(); });
+  afterEach(async () => { await page.close(); }); 
+
+  it('works', async () => {
+   await page.goto('http://localhost:5500/Accordion');
+   await page.screenshot({ path: 'site.png' })
+  })
+});
